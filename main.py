c6bc47712be224985b73726ed79074a621ce73df
Nightingale <114455188+NightingaleA@users.noreply.github.com>
2022-09-27 04:35:13 +0000

    Created main, connected bot and left it running

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+import discord
+from discord.ext import commands
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+bot.run(TOKEN)
